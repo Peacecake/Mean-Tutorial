bbb2dd29775be05941f6d31f23641e400bb7491b
Peacecake <sebastian.peiser@stud.uni-regensburg.de>
2017-11-09 19:19:23 +0100

    express setup and routes

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require ("express");
+const path  =require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+// Connect to mongodb database
+mongoose.connect(config.database);
+mongoose.connection.on("connected", () => {
+  console.log("connected to database " + config.database);
+});
+mongoose.connection.on("error", (err) => {
+  console.log("database error " + err);
+});
+
+const app = express();
+
+const users = require("./routes/users");
+
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use("/users", users);
+
+// index route
+app.get("/", (request, response) => {
+  response.send("invalid endpoint");
+});
+
+// Start server
+app.listen(port, () => console.log("Server startet on port " + port));
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+
+// Register
+router.get("/register", (req, res, next) => {
+  res.send("register");
+});
+
+// Authenticate
+router.get("/authenticate", (req, res, next) => {
+  res.send("authenticate");
+});
+
+// Profile
+router.get("/profile", (req, res, next) => {
+  res.send("profile");
+});
+
+// Validate
+router.get("/validate", (req, res, next) => {
+  res.send("validate");
+});
+
+module.exports = router;

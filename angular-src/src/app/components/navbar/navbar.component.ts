f46a8d63fe621bb93f026bc421074df41dda4867
Peacecake <sebastian.peiser@stud.uni-regensburg.de>
2017-11-17 21:56:14 +0100

    Login and Logout

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +12,19 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService : AuthService,
+    private router : Router,
+    private flashMessage : FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.show('You are logged out', {cssClass: "alert-success", timeout: 3000});
+    this.router.navigate(['/login']);
+    return false;
   }
 
 }
